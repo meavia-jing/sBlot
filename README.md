470d67ca475d5c0a4062276f9f57f123fb02d19e
anjing <116026859+maeva-jing@users.noreply.github.com>
2022-11-25 17:25:45 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# SBbaysPlot
+ This package provide plotting functions for the project Sbayes
+
+
+
+## configuration file
+map >> conten >> type
+if the type == "density_map" , plot all the languages
+if the type == "consensus_map", plot languages with afrequency larger than min_posterior_frequency
+
+graphic >> clusters>> size
+if the type of size is a number , plot line graph, all languages with the same point size
+if point_size = “frequency” plot point size according to frequency
+if point_size = "both" plot the point size accroding to frequency and the line graph.
+
+
---
+++ plot_balkan.py
@@ -0,0 +1,20 @@
+import warnings
+from plot import Plot
+
+import os
+
+if __name__ == '__main__':
+    plot = Plot()
+    # plot.load_config(config_file='experiments/balkan/config_plot_empty_map.json')
+    plot.load_config(config_file='config_plot.json')
+    plot.read_data()
+    #path = '/Users/zhanganjing/Downloads/RA work/sbayes_plot/results'
+    for name, result in plot.iterate_over_models():
+        ## plot map
+        plot.posterior_map(results=result, file_name=f'map_{name}')
+        #plot.plot_pies(result,os.path.join(path,name))
+
+    plot.plot_dic(models=plot.results, file_name='dic')
+
+
+

4f1982dead5a1bf7bd93651f767ddc9a65268353
anjing <116026859+maeva-jing@users.noreply.github.com>
2023-03-06 17:05:39 +0100

    Add files via upload

--- plot_balkan.py
+++ plot_balkan.py
@@ -2,20 +2,29 @@ import warnings
 from sbayes.plot import Plot
 
 import os
+import numpy as np
+from matplotlib import colors
 
 if __name__ == '__main__':
     plot = Plot()
     # plot.load_config(config_file='experiments/balkan/config_plot_empty_map.json')
-    plot.load_config(config_file='config_plot.json')
+    plot.load_config(config_file='/Users/zhanganjing/Downloads/RA/SbayesPlot/config_plot_south_america.json')
     plot.read_data()
+
+
     for name, result in plot.iterate_over_models():
-        ## plot map
-        #plot.posterior_map(results=result, file_name=f'map_{name}')
+        #plot.plot_weights(result,name)
+        plot.plot_features(result,name)
+        #plot.get_idw_map(result, name)
+    #     #plot map
+         #plot.posterior_map(results=result, file_name=f'map_{name}')
 
-        #plot.plot_pies(result,os.path.join(path,name))
-        plot.get_idw_map(result,name)
+          #plot.plot_preferences(result,name)
+    #
+         #plot.plot_pies(result,os.path.join(name))
 
-    plot.plot_dic(models=plot.results, file_name='dic')
+
+    #plot.plot_dic(models=plot.results, file_name='dic')
 
 
 

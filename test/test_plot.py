d0d45d4b30bf0439decd6783bfa1c83b58eafe27
anjing <116026859+maeva-jing@users.noreply.github.com>
2023-05-12 15:58:58 +0200

    Add files via upload

--- test/test_plot.py
+++ test/test_plot.py
@@ -94,24 +94,24 @@ class TestPlot(unittest.TestCase):
         for map_path in map_paths:
             assert os.path.exists(map_path)
             assert os.path.getsize(map_path) > 0
-
-
-    def test_example_feature_plot(self):
-        main(
-            config='plot_test_files/config_plot_preference.json',
-            plot_types=[PlotType.feature_plot],
-            feature_name="F1"
-        )
-        map_paths = [
-            "results/preference/prob_grid_K1_0_family_Arawak.pdf",
-            "results/preference/prob_grid_K3_0_family_Arawak.pdf",
-        ]
-        for map_path in map_paths:
-            assert os.path.exists(map_path)
-            assert os.path.getsize(map_path) > 0
-
-
     #
+    #
+    # def test_example_feature_plot(self):
+    #     main(
+    #         config='plot_test_files/config_plot_preference.json',
+    #         plot_types=[PlotType.feature_plot],
+    #         feature_name="F1"
+    #     )
+    #     map_paths = [
+    #         "results/preference/prob_grid_K1_0_family_Arawak.pdf",
+    #         "results/preference/prob_grid_K3_0_family_Arawak.pdf",
+    #     ]
+    #     for map_path in map_paths:
+    #         assert os.path.exists(map_path)
+    #         assert os.path.getsize(map_path) > 0
+
+
+
     def test_example_dic(self):
         main(
             config='plot_test_files/config_plot_preference.json',
@@ -123,58 +123,39 @@ class TestPlot(unittest.TestCase):
         for map_path in map_paths:
             assert os.path.exists(map_path)
             assert os.path.getsize(map_path) > 0
+    #
+    # def test_example_pie(self):
+    #     main(
+    #         config='plot_test_files/config_plot_pie.json',
+    #         plot_types=[PlotType.pie_plot]
+    #     )
+    #     map_paths = [
+    #         "results/pie/plot_pies_K1_0.pdf",
+    #         "results/pie/plot_pies_K3_0.pdf",
+    #     ]
+    #     for map_path in map_paths:
+    #         assert os.path.exists(map_path)
+    #         assert os.path.getsize(map_path) > 0
+    #
+    #
+    #
+    #
 
-    def test_example_pie(self):
-        main(
-            config='plot_test_files/config_plot_pie.json',
-            plot_types=[PlotType.pie_plot]
-        )
-        map_paths = [
-            "results/pie/plot_pies_K1_0.pdf",
-            "results/pie/plot_pies_K3_0.pdf",
-        ]
-        for map_path in map_paths:
-            assert os.path.exists(map_path)
-            assert os.path.getsize(map_path) > 0
-
-
-
-
-    def test_example_idw_map(self):
-        main(
-            config='plot_test_files/config_plot.json',
-            plot_types=[PlotType.map]
-        ,
-        )
-        map_paths = [
-            "results/map/posterior_map_K1_0_0.7.pdf",
-            "results/map/posterior_map_K3_0_0.7.pdf",
-        ]
-        for map_path in map_paths:
-            assert os.path.exists(map_path)
-            assert os.path.getsize(map_path) > 0
-
-
-    def test_compute_bbox(self):
-        extent = {'x_min': -100000,
-                  "x_max": 1500000,
-			      "y_min": 1040000,
-                  'y_max':2000000 }
-        test_bbox = geometry.box(extent['x_min'], extent['y_min'],
-                            extent['x_max'], extent['y_max'])
-        plot = Plot()
-        bbox = plot.compute_bbox(extent)
-        self.assertEqual(bbox,test_bbox)
-
-    def test_get_cluster_colors_five(self):
-        custom_colors =  ["#1b9e77","#d95f02","#7570b3","#e7298a","#66a61e"]
-        test_color = np.array([colors.to_rgba(c) for c in custom_colors])
-
-        plot = Plot()
-        color = plot.get_cluster_colors(n_clusters=5,custom_colors=custom_colors)
-        self.assertTrue((color==test_color).all())
-        #self.assertTrue(color==test_color)
-
+    #
+    #
+    # def test_compute_bbox(self):
+    #     extent = {'x_min': -100000,
+    #               "x_max": 1500000,
+	# 		      "y_min": 1040000,
+    #               'y_max':2000000 }
+    #     test_bbox = geometry.box(extent['x_min'], extent['y_min'],
+    #                         extent['x_max'], extent['y_max'])
+    #     plot = Plot()
+    #     bbox = plot.compute_bbox(extent)
+    #     self.assertEqual(bbox,test_bbox)
+    #
+    #
+    #
     # def test_get_cluster_colors_seven(self):
     #     cm = plt.get_cmap('gist_rainbow')
     #     custom_colors =  ["#1b9e77","#d95f02","#7570b3","#e7298a","#66a61e"]
@@ -185,8 +166,8 @@ class TestPlot(unittest.TestCase):
     # #     plot = Plot()
     # #     color = plot.get_cluster_colors(n_clusters=7,custom_colors=custom_colors)
     # #     self.assertTrue(np.isin(color,test_color).all())
-
-
+    #
+    #
     # def test_get_cluster_colors_six(self):
     #     custom_colors =  ["#1b9e77","#d95f02","#7570b3","#e7298a","#66a61e"]
     #     test_color = np.array([colors.to_rgba(c) for c in custom_colors])
